291b7f463a84805e7dcfc848f5e9fbe75f84d6da
Rafael088 <jerafa088@gmail.com>
2022-11-11 16:45:06 -0500

    fix interface

--- src/components/taller.js
+++ src/components/taller.js
@@ -1,10 +1,7 @@
+
+import React, { useState } from 'react';
+
 function Taller() {
-    /*
-        Utiliza una forma simple de sustitución polialfabética 
-        La tabla consta de los alfabetos escritos 26 veces en diferentes filas, cada alfabeto desplazado cíclicamente hacia la izquierda en comparación con el alfabeto anterior, correspondiente a los 26 Cifrados César posibles .
-        En diferentes puntos del proceso de cifrado, el cifrado utiliza un alfabeto diferente de una de las filas.
-        El alfabeto utilizado en cada punto depende de una palabra clave repetitiva.
-    */
 
     //genera la key de la plabra
     function generateKey(str,key){
@@ -22,39 +19,33 @@ function Taller() {
             }
             return key.join("");
     }
-    //s
+    
+    //encripta la palabra
     function cipherText(str,key){
         let cipher_text="";
     
-        for (let i = 0; i < str.length; i++)
-        {
-            // converting in range 0-25
+        for (let i = 0; i < str.length; i++){
             let x = (str[i].charCodeAt(0) + key[i].charCodeAt(0)) %26;
     
-            // convert into alphabets(ASCII)
             x += 'A'.charCodeAt(0);
     
             cipher_text+=String.fromCharCode(x);
         }
         return cipher_text;
     }
-    
+    //desencripta la palabra
     function originalText(cipher_text,key){
         let orig_text="";
       
-        for (let i = 0 ; i < cipher_text.length ; i++)
-        {
-            // converting in range 0-25
+        for (let i = 0 ; i < cipher_text.length ; i++){
             let x = (cipher_text[i].charCodeAt(0) -
                         key[i].charCodeAt(0) + 26) %26;
-      
-            // convert into alphabets(ASCII)
             x += 'A'.charCodeAt(0);
             orig_text+=String.fromCharCode(x);
         }
         return orig_text;
     }
-
+    //mayusculas y minusculas
     function LowerToUpper(s){
         let str =(s).split("");
         for(let i = 0; i < s.length; i++)
@@ -68,18 +59,47 @@ function Taller() {
         return s;
     }
 
-    let str = "JUANESTEBAN";
-    let keyword = "CLAVE";
+    const [str, setStr] = useState("");
+    const [keyword, setKeyword] = useState("A");
     
     
     let key = generateKey(str, keyword);
     
     let cipher_text = cipherText(str, key);
 
+
+    const [desCipeh, setDesCipeh] = useState("");
+    let desciphe = originalText(desCipeh, key);
+
+    const handleChange = (e) => {
+
+        setStr(e.target.value)
+    }
+    const handleChanges = (e) => {
+
+        setKeyword(e.target.value)
+    }
+    const handleChangeDes = (e) => {
+
+        setDesCipeh(e.target.value)
+    }
     return ( 
         <div className="contB">
-            <h2>Texto Encriptado con kasiski</h2>
+            <h2>Encriptado con kasiski</h2>
+            <p>Solo funciona con mayusculas</p>
+            
+            <p>Clave</p>
+            <input type="text" onChange={handleChanges} />
+
+            <p>Palabra</p>
+            <input type="text" onChange={handleChange} />
+
+            <b>Texto encriptado</b>
             <p>{cipher_text}</p>
+            <b>Texto desencriptado</b>
+            <p>Palabra</p>
+            <input type="text" onChange={handleChangeDes} />
+            <p>{desciphe}</p>
         </div>
      );
 }
